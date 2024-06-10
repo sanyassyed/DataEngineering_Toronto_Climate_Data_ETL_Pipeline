f0c3dbdbbe001d76f3e1c95c89b3df070963ffdb
Ubuntu Sarah DELab <ubuntu@ip-172-31-45-230>
2024-06-10 11:28:11 +0000

    CICD: File download complete

--- extract.sh
+++ extract.sh
@@ -14,8 +14,20 @@ year=(2020 2021 2022)
 month=2
 day=14
 # timeframe 1:hourly data 2:daily data 3:monthly data
-timeframe=3 
+timeframe=2 
 
-website="https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=${format}&stationID=${stationId}&Year=${year}&Month=${month}&Day=${day}&timeframe=${timeframe}&submit=Download+Data"
 
-wget -O ${website}
+
+#simple
+#website="https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=${format}&stationID=${stationId}&Year=${year}&Month=${month}&Day=${day}&timeframe=${timeframe}&submit=Download+Data"
+# --content-disposition looks into the header for an appropriate file name
+#wget --content-disposition ${website}
+# to give your own file name
+#wget ${website} -O test.csv 
+
+for y in ${year[@]}
+do
+    website="https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=${format}&stationID=${stationId}&Year=${y}&Month=${month}&Day=${day}&timeframe=${timeframe}&submit=Download+Data"
+    # to supress the download messages and perform download in quiet mode
+    wget -q --content-disposition ${website}
+done
